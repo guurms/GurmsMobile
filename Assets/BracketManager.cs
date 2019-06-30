29b826c2cea43d0bc3910453ecf305c7e987565f
unknown <hjorturjohann94@gmail.com>
2019-06-30 16:31:54 +0000

    Somewhat playerable prototype in Hjortur scene

---
+++ Assets/BracketManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BracketManager : MonoBehaviour
+{
+    private float spawnTimer;
+
+    public GameObject bracketPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+
+        if(spawnTimer < 0)
+        {
+            SpawnBracket();
+            spawnTimer = 0.6f;
+        }
+    }
+
+    private void SpawnBracket() 
+    {
+        //Want to pull from the bracketPool and place the object in the right spot and enable it
+        Instantiate(bracketPrefab, new Vector3(Random.Range(-1f, 2f), -6, 0), Quaternion.identity, transform);
+    }
+}
---
+++ Assets/PlayerCollisionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col) 
+    {
+        transform.SetParent(col.transform);
+    }
+}
