d3fd812b555f372337155f65523bf93e81415ea7
Petra Frantz <petra091@gmail.com>
2019-07-31 20:48:04 +0000

    blob

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour
+{
+    //Play the game 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ToggleSound()
+    {
+        Debug.Log("this script will turn on/off the sound");
+    }
+}
