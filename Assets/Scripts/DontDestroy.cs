eb44dae8668409326d623b623ffc1bd9dc8b2ca0
Petra Frantz <petra091@gmail.com>
2019-08-04 22:29:57 +0000

    settings volume created

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DontDestroy : MonoBehaviour
+{
+    AudioSource audio;
+    public Slider volume;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void ToggleMusic()
+    {
+        audio.mute = !audio.mute;
+    }
+
+    public void VolumeSlider()
+    {
+        audio.volume = volume.value;
+    }
+}
